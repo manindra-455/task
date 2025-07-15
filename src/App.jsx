547e76c2289b026cac888a6e4f7c7c53e15b5413
AnujNainwal69 <anainwal7@gmail.com>
2025-07-15 20:46:22 +0530

    Resolved merge conflict in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import ProtectedRoute from "./router/ProtectedRoute";
 import MobileBlockPage from "./utility/MobileBlockPage";
 import Login from "./pages/Login";
 import ContactPage from "./pages/ContactPage";
+import ProjectPage from "./components/Project/ProjectPage";
+import Plan from "./pages/Plan";
 
 function App() {
   return (
@@ -25,10 +27,12 @@ function App() {
       >
         <Route index element={<Dashboard />} />
         <Route path="projects" element={<Projects />} />
+        <Route path="projects/view" element={<ProjectPage />} />
         <Route path="calendar" element={<Calendar />} />
         <Route path="chat" element={<Chat />} />
         <Route path="team" element={<Team />} />
-        <Route path="contact" element={<ContactPage/>} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="plan" element={<Plan />} />
       </Route>
     </Routes>
   );
